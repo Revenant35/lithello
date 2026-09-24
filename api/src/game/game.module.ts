e4bfac893dd28230180390b98250238198e48936
Zach Brown <bbrown64506@gmail.com>
2026-09-24 10:55:12 -0500

    fix: local GameSessionService ref that snuck in

--- api/src/game/game.module.ts
+++ api/src/game/game.module.ts
@@ -4,11 +4,10 @@ import { RedisModule } from "../redis/redis.module.ts";
 import { GameRepository } from "./game.repository.ts";
 import { GameService } from "./game.service.ts";
 import { GameSessionRepository } from "./game-session.repository.ts";
-import { GameSessionService } from "./game-session.service.ts";
 
 @Module({
   imports: [DatabaseModule, RedisModule],
-  providers: [GameRepository, GameService, GameSessionRepository, GameSessionService],
-  exports: [GameService, GameSessionService],
+  providers: [GameRepository, GameService, GameSessionRepository],
+  exports: [GameService],
 })
 export class GameModule {}
